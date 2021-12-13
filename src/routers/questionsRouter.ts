3e4a26b00206e0f5be4dbe5bd14a184dfa784fed
Francisca <fraan928@gmail.com>
2021-12-13 03:10:08 -0300

    Determining routes

--- src/routers/questionsRouter.ts
+++ src/routers/questionsRouter.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import * as answersController from '../controllers/answersController';
 import * as questionsController from '../controllers/questionsController';
 
 const router = express.Router();
 
 router.post('/', questionsController.postQuestions);
+router.get('/', questionsController.findUnansweredQuestions);
+router.post('/:id', answersController.postAnswer);
+router.get('/:id', questionsController.findQuestionById);
+// router.post('/users',);
 
 export default router;
